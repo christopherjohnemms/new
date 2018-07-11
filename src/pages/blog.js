4d503d4266bcedbb686857e13f95a68716860e96
Chris Emms <christopher.emms@mms-marketing.co.uk>
2018-07-11 11:05:00 +0100

    small fixes to blogposting

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styles from '../assets/blogposts.module.scss'
 
 
 const BlogPage = ({data}) => (
-    <div>
+    <div className={styles.pagecontent}>
         <h1>Blog Page</h1>
         <p>Lorem Ipsum</p>
         <Link to="/">Go back to the homepage</Link>
@@ -11,7 +12,7 @@ const BlogPage = ({data}) => (
 
         <div>
             {data.allMarkdownRemark.edges.map(({node}) => (
-                <div key={node.id} className="article-box">
+                <div key={node.id} className={styles.articlebox}>
                     <Link to={node.fields.slug} style={{textDecoration: 'none', color: 'inherit'}}><h3 className="title">{node.frontmatter.title}</h3></Link>
                     <p className="author">{node.frontmatter.author}</p>
                     <p className="date">{node.frontmatter.date}</p>
