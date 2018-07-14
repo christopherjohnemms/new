7f96ecf5a5fe15f6ec13895af85e84646509b520
Chris Emms <christopherjohnemms@gmail.com>
2018-07-14 15:50:31 +0100

    forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,8 +5,51 @@ import styles from '../assets/contact.module.scss'
 
 const ContactPage = () => (
     <div className={styles.pagecontent}>
-        <h1>Contact Page</h1>
-        <p>Lorem Ipsum</p>
+        <h1>We <span className={styles.pink}>can't wait</span> to hear from you</h1>
+        <h2>Use the form below to drop us a message, whether its an 
+            enquiry or a friendly chat, we don't mind! </h2>
+        
+        
+        <div className={styles.formcontent}>
+            
+            <div>
+            
+                <p>For all enquiries or questions, please don't hesitate to use the form on this page to instantly get in contact with us through email.</p>
+                
+                <p>If you'd like to get in contact with us in other ways, then we have our own Facebook page which you can message us on, or alternatively you can drop us a message on our Instagram page.</p>
+                
+                <p>If you prefer to contact us by phone, please give us a call on 07972345095. If we are busy or unable to take your call at that moment, please use the voicemail service and let us know your details so we can get back to you as soon as possible!</p>
+            </div>
+            
+            <div className={styles.padding}>
+                <form name="contact" method="POST" action="/success" netlify>
+                     <p>
+                        <label>Your Name: <br /> <input type="text" name="name" /></label>   
+                      </p>
+                        <p>
+                        <label>Your Email: <br /><input type="email" name="email" /></label>
+                      </p>
+                      <p>
+                        <label>Phone Number:<br /> <input type="text" name="phone" /></label>
+                      </p>
+                     <p>
+                        <label>Wedding Date (if you have one!)<br /> <input type="text" name="wedding_date" /></label>
+                      </p>
+                        <p>
+                        <label>Location (if you have one!) <br /><input type="text" name="location" /></label>
+                      </p>
+                      <p>
+                        <label>What can we do for you?:<br /> <textarea name="message"></textarea></label>
+                    </p>
+                      <p>
+                        <button type="submit">Send</button>
+                      </p>
+
+                </form>
+            </div>
+        </div>
+        
+        
         <Link to="/">Go back to the homepage</Link>
     </div>
 )
