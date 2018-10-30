d657e47408d49c699c520c0492857220f047a92d
Chris Emms <christopherjohnemms@gmail.com>
2018-10-30 21:19:20 +0000

    a

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,21 +5,20 @@ import styles from '../assets/blogposts.module.scss'
 
 const BlogPage = ({data}) => (
     <div className={styles.pagecontent}>
-        <h1>Blog Page</h1>
-        <p>Currently migrating blog posts from the old site</p>
-        <Link to="/">Go back to the homepage</Link>
-
-
-        <div className={styles.hidden}>
+        <h1>Our Blog</h1>
+  
+        <div className={styles.blogposts}>
             {data.allMarkdownRemark.edges.map(({node}) => (
                 <div key={node.id} className={styles.articlebox}>
-                    <Link to={node.fields.slug} style={{textDecoration: 'none', color: 'inherit'}}><h3 className="title">{node.frontmatter.title}</h3></Link>
-                    <p className="author">{node.frontmatter.author}</p>
-                    <p className="date">{node.frontmatter.date}</p>
-                    <p className="excerpt">{node.excerpt}</p>
+                    <Link to={node.fields.slug} style={{textDecoration: 'none', textAlign:'center'}}><h2 className="title">{node.frontmatter.title}</h2></Link>
+  <div className={styles.divider}></div>
+                    <p style={{textDecoration: 'none', textAlign:'center'}} className="excerpt">{node.excerpt}</p>
                 </div>
             ))}
         </div>
+
+        <Link to="/">Go back to the homepage</Link>
+        
     </div>
 )
 
