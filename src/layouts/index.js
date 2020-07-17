5b2c6cb8fefb442e6fc81aae129793c1171e4cd9
Christopher Emms <christopherjohnemms@gmail.com>
2020-07-17 22:08:16 +0100

    a

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -41,50 +41,9 @@ const Layout = ({ children, data }) => (
       <div className={styles.body}>
           {children()}
       </div>
-        
-    <div className={styles.social}>
-        <div>
-            <p>We'd <span className={styles.pink}>love</span> for you to get in touch!</p>
-        </div>
-        <div className={styles.icons}>
-            <div className={styles.icon}>
-                <a href="https://www.facebook.com/emberphotographyuk/" target="_blank">
-                    <Img
-                        title="Ember Photography Facebook "
-                        alt="Ember Photography Facebook"
-                        sizes={data.FacebookIcon.sizes}
-                    />
-                </a>
-            </div>
-            <div className={styles.icon}>
-                <a href="https://www.instagram.com/emberphotographyuk/" target="_blank">
-                    <Img
-                        title="Ember Photography Instagram"
-                        alt="Ember Photography Instagram"
-                        sizes={data.InstagramIcon.sizes}
-                    />
-                </a>
-            </div>
-            <div className={styles.icon}>
-                <a href="contact">
-                    <Img
-                        title="Ember Photography Book Us"
-                        alt="Ember Photography Book Us"
-                        sizes={data.ContactIcon.sizes}
-                    />
-                </a>
-            </div>
-        </div>
-    </div>
-    <div className={styles.footer}>
-        <div className={styles.right}>
-            <p>Email: <a href="mailto:info@ember.photography">info@ember.photography</a></p>
-            <p>Phone: <span>07972345095</span></p>
-            <small>Copyright Ember Photography</small>
-        </div>
-    </div>
   </div>
 )
+
 
 Layout.propTypes = {
   children: PropTypes.func,
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,85 +9,10 @@ class IndexPage extends React.Component {
   render() {
     return (
       <div>
-          <div className={"test"}>
-              <div className={"test"}></div>
-          </div>
-          <SlickSlider
-            slideImageOne=      {this.props.data.slideImageOne}
-            slideImageTwo=      {this.props.data.slideImageTwo}
-            slideImageThree=    {this.props.data.slideImageThree}
-            slideImageFour=     {this.props.data.slideImageFour}
-            slideImageFive=     {this.props.data.slideImageFive}
-            slideImageSix=     {this.props.data.slideImageSix}
-          />
-          <div className={styles.content}>
-              <h1><span className={styles.pink}> Story tellers</span> first.</h1>
-              <p>We thrive on capturing those amazing and unique moments that happen when people forget the cameras are there. When you hire a wedding photographer you are inviting someone into one of the most precious chapters of your life and trusting them to tell this part of your story. We stay true to our vision by using a photojournalistic style to document those fleeting moments, providing a true reflection of your wedding day.</p>
-          </div>
-        
-            <div className={styles.us}>
-                <Img
-                        title="Chris and Sam"
-                        alt="Chris and Sam"
-                        sizes={this.props.data.UsImage.sizes}
-                    />
-                
-                
-            </div>
-            
-            <div className={styles.content}>
-              <h1>We are <span className={styles.pink}> Sam & Chris</span>.</h1>
-              <p>We're two ambitious, passionate wedding photographers from Staffordshire looking to capture the unparalelled memories
-              your day will bring. We thrive on the wedding atmosphere, bringing with us a focused and professional approach based on our foundations as
-              creative people.</p>
-                <div className={styles.bookus}>
-                    <Link to="/contact/">BOOK US</Link> <span> | </span><Link to="/portfolio/">LEARN MORE</Link>
-                </div>
-            </div>
+        Maintenance
       </div>
     );
   }
 }
 
 export default IndexPage
-
-export const pageQuery = graphql`
-  query SliderImageQuery {
-    slideImageOne: imageSharp(id: { regex: "/home/1.jpg/" }) {
-      sizes(maxWidth: 2400, quality:95  ) {
-        ...GatsbyImageSharpSizes
-      }
-    },
-    slideImageTwo: imageSharp(id: { regex: "/home/2.jpg/" }) {
-      sizes(maxWidth: 2400, quality:95 ) {
-        ...GatsbyImageSharpSizes
-      }
-    },
-    slideImageThree: imageSharp(id: { regex: "/home/3.jpg/" }) {
-      sizes(maxWidth: 2400, quality:95 ) {
-        ...GatsbyImageSharpSizes
-      }
-    },
-    slideImageFour: imageSharp(id: { regex: "/home/4.jpg/" }) {
-      sizes(maxWidth: 2400, quality:95 ) {
-        ...GatsbyImageSharpSizes
-      }
-    },
-    slideImageFive: imageSharp(id: { regex: "/home/5.jpg/" }) {
-      sizes(maxWidth: 2400, quality:95 ) {
-        ...GatsbyImageSharpSizes
-      }
-    },
-    slideImageSix: imageSharp(id: { regex: "/home/6.jpg/" }) {
-      sizes(maxWidth: 2400, quality:95 ) {
-        ...GatsbyImageSharpSizes
-      }
-    },
-    UsImage: imageSharp(id: { regex: "/home/us.jpg/" }) {
-      sizes(maxWidth: 1200 ) {
-        ...GatsbyImageSharpSizes_tracedSVG
-      }
-    },
-  },
-
-`
