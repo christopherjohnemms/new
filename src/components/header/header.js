216f353d190e9783d33b0cab9c7ea3e241184a61
Christopher Emms <christopherjohnemms@gmail.com>
2020-07-17 22:14:45 +0100

    a

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,26 +7,26 @@ import styles from '../../assets/header.module.scss'
 
 const Header = ({ siteTitle, HeaderImage }) => (
     <div className={styles.header_wrap}>
-        <div className={styles.header_inner}>
-            <div className={styles.logo}>
-                <Link to="/">
-                    <Img
-                        title="Ember Photography"
-                        alt="Ember Photography"
-                        sizes={HeaderImage.sizes}
-                    />
-                </Link>
-            </div>
-            <div className={styles.links}>
-                <ul>
-                      <li><Link to="/portfolio/">Portfolio</Link></li>
-                      <li><Link to="/about/">About</Link></li>
-                      <li><Link to="/prices/">Prices</Link></li>
-                      <li><Link to="/blog/">Blog</Link></li>
-                      <li><Link to="/contact/">Contact</Link></li>
-                </ul>
-            </div>
-        </div>
+        {/*<div className={styles.header_inner}>*/}
+        {/*    <div className={styles.logo}>*/}
+        {/*        <Link to="/">*/}
+        {/*            <Img*/}
+        {/*                title="Ember Photography"*/}
+        {/*                alt="Ember Photography"*/}
+        {/*                sizes={HeaderImage.sizes}*/}
+        {/*            />*/}
+        {/*        </Link>*/}
+        {/*    </div>*/}
+        {/*    <div className={styles.links}>*/}
+        {/*        <ul>*/}
+        {/*              <li><Link to="/portfolio/">Portfolio</Link></li>*/}
+        {/*              <li><Link to="/about/">About</Link></li>*/}
+        {/*              <li><Link to="/prices/">Prices</Link></li>*/}
+        {/*              <li><Link to="/blog/">Blog</Link></li>*/}
+        {/*              <li><Link to="/contact/">Contact</Link></li>*/}
+        {/*        </ul>*/}
+        {/*    </div>*/}
+        {/*</div>*/}
     </div>
 )         
 
